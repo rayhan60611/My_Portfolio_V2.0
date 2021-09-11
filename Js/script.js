819443591ffd462da18b49a7812356b613acaa1e
M M Rayhan Parvez <pecon.alok@gmail.com>
2021-09-12 00:48:08 +0600

    my latest V 2.0 Portfolio my teams Cards

--- Js/script.js
+++ Js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 
 
 
-    //toggle menu/navbar//
+    //////////////toggle menu/navbar scripts////////////
 
     $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass("active");
@@ -27,7 +27,28 @@ $(document).ready(function() {
 
 
 
+    /////////////// owl carosel scripts////////////////
 
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
 
 
 
